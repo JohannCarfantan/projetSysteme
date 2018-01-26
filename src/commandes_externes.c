0c4b7b771070cbdac645dfa89123028d329fa373
Unknown <julien.gestin35400@gmail.com>
2018-01-26 14:21:04 +0100

    Redirection vers fichier en commandes externes OK

--- src/commandes_externes.c
+++ src/commandes_externes.c
@@ -1,6 +1,8 @@
 #include "divers.h"
 #include "commandes_externes.h"
 #include <sys/wait.h>
+#include <fcntl.h>    /* For O_RDWR */
+#include <unistd.h>
 
 t_bool	ActionEXEC (parse_info *info, int debut, int nbArg) {
   char ligne[CHAINE_MAX];
@@ -35,15 +37,31 @@ t_bool	ActionEXEC (parse_info *info, int debut, int nbArg) {
     printf("arg : %s \n", arg[i]);
     }
 
+    char *file = info->sortie;
+
+    printf("File %s \n",file);
+
     pid_t pid_fils = fork();
     if ( pid_fils == -1) {
     perror ("Echec du fork 1\n");
     return EXIT_FAILURE;
     }
     if( pid_fils ==0){
+
+      if(file!=NULL)
+      {
+      int fd = open(file, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+
+      dup2(fd, 1);   // make stdout go to file
+      dup2(fd, 2);   // make stderr go to file - you may choose to not do this
+
+      close(fd);
+      }
+
       int res = execvp(*(&arg[0]),&arg[0]);
       (void) res;
       //printf("Res CE : %d \n", res);
+
     }
 
   //printf("execution d'une commande externe  (%s %d) a ecrire :\n%s\n", __FILE__, __LINE__, ligne);
